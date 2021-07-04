0099656a79db7c04d5e470b2620dbd4ecb51358b
Tasnimwheebi <tasnimwheebi@gmail.com>
2021-07-04 23:38:20 +0300

    redux-1

---
+++ my-app/src/store/categories.js
@@ -0,0 +1,33 @@
+const initialState = {
+    categories:[
+        {
+            name:'electronics',
+            displayName:'ELECTRONIC',
+            description:'tv'
+        },
+        {
+            name:'food',
+            displayName:'FOOD',
+            description:'rice'
+        },
+    ],
+    activeCategory:'electronics'
+}
+export default (state = initialState, action) => {
+    let { type, payload } = action;
+    switch (type) {
+        case 'change':
+          let activeCategory = payload.name;
+          console.log('ACTIVE CATEGORY: ', activeCategory);
+          return { ...state, activeCategory };
+        default:
+          return state;
+      }
+};
+
+export const changeActiveCategory = (name) => {
+    return {
+      type: 'change',
+      payload: name,
+    };
+  };
