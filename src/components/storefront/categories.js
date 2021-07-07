ae72773334875e59c24fbd554c48ee7eae2a35ba
Tasnimwheebi <tasnimwheebi@gmail.com>
2021-07-07 19:11:39 +0300

    lab-38

--- src/components/storefront/categories.js
+++ src/components/storefront/categories.js
@@ -2,22 +2,22 @@ import React from 'react';
 import { connect } from 'react-redux';
 // import { changeActiveCategory } from '../../store/products';
 import { Button } from '@material-ui/core';
+import { useEffect } from 'react';
 import { getRemoteData } from '../../store/categories'; // action function from the reducer
+import { getData } from '../../store/products';
 import { useDispatch} from 'react-redux'; // add methods from redux
 const Categories = (props) => {
   const dispatch = useDispatch(); /// use the method useDispatch
-  // const state = useSelector((state) => {
-  //     return { categoryReducer: state.categoryReducer }
-  // }); /// return data from the reducer
-    // console.log(props.categories);
-   
+  useEffect( ()=>{
+   dispatch(getRemoteData());
+  },[])
   return (
     <div>
         <ul>
           {props.categories.map((category) => {
             return (
-              <Button color="primary" key={category.displayName} onClick={() => dispatch(getRemoteData(category.name))}>
-                {category.displayName}
+              <Button  onClick={()=>{dispatch(getData(category.name))}} color="primary" key={category.name}>
+                {category.name}
               </Button>
             );
           })}
--- src/store/categories.js
+++ src/store/categories.js
@@ -1,74 +1,27 @@
 import superagent from 'superagent';
 const initialState = {
-    categories:[
-        {
-            name:'electronics',
-            displayName:'ELECTRONIC',
-            description:'Category Description Goes Here'
-        },
-        {
-            name:'food',
-            displayName:'FOOD',
-            description:'Category Description Goes Here'
-        },
-        {
-            name:'phones',
-            displayName:'PHONE',
-            description:'Category Description Goes Here'
-        },
-        {
-            name:'shirts',
-            displayName:'SHIRTS',
-            description:'Category Description Goes Here'
-        },
-
-    ],
+    categories:[],
    
 }
  const categoryReducer=  (state = initialState, action) => {
     let { type, payload } = action;
     switch (type) {
-        // case 'DISPLAY':
         case 'GET':
-        let active = payload;
-        // console.log('data from api',payload.results);
-         payload.results.map(items=>{
-             console.log(items.category);
-             return items.category;
-         })
-        let description=state.categories[0].description;
-            return { categories:state.categories,act:active , description:description };
-    // case 'RESET':
-    //     return initialState;
+            return { categories:payload.results };
     default:
         return state;
 };
-
  }
-
- 
-
- 
  export const getRemoteData = (name) => (dispatch, state) => {
- const api = `https://api-js401.herokuapp.com/api/v1/products?category=${name}`;
+ const api = `https://api-js401.herokuapp.com/api/v1/categories`;
     return superagent.get(api).then(res => {
-        dispatch(getAction(res.body));
-        
+        dispatch(getAction(res.body));  
     });
 }
 export const getAction = payload => {
     return {
         type: 'GET',
         payload,
-        
     }
 }
-
-//   export const changeCategory = (name) => {
-//     return {
-//         type: 'ACTIVE',
-//         payload: name
-//     }
-//   };
-
 export default categoryReducer;
